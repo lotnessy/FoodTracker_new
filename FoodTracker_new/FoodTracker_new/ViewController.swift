0d88a24c0aa1b2ad34cacb67b3e41a2a82cbe936
Natalia Yakunina <yakunina.natalia.y@gmail.com>
2019-11-05 13:04:06 +0300

    init commit

---
+++ FoodTracker_new/FoodTracker_new/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker_new
+//
+//  Created by Наталья Якунина on 05/11/2019.
+//  Copyright © 2019 Наталья Якунина. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
